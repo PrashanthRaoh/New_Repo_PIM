b10d4417cb3de2fb1d6c85a8e0e403f892b80526
Prashanth Rao <raoad@timken.com>
2024-12-09 11:22:40 +0530

    pim version change

--- src/test/java/pim_Selenium4_Automation/GetPIM_Version.java
+++ src/test/java/pim_Selenium4_Automation/GetPIM_Version.java
@@ -1,5 +1,6 @@
 package pim_Selenium4_Automation;
 
+import pages.HomePage;
 import pages.Login_Page;
 import java.io.IOException;
 
@@ -13,7 +14,11 @@ public class GetPIM_Version extends BaseTest {
 	public void GetVersion() throws IOException {
 		String className = this.getClass().getSimpleName();
 		System.out.println(className);
-//		Login_Page loginPage = new Login_Page(driver);
-//		loginPage.LogintoPIM();
+		Login_Page loginPage = new Login_Page(driver);
+		loginPage.LogintoPIM();
+		HomePage homePage = new HomePage(driver);
+		
+		utils.waitForElement(homePage.HomePage_SearchButton(), "clickable");
+		utils.Takescreenshot();
 	}
 }
